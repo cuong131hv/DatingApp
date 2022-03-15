10d415055948cad218350f724f15d67d8f7a4994
cuong131hv <cuong131hv@gmail.com>
2022-03-15 17:33:51 +0700

    init commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        //khoi tao field de tra lai du lieu len context
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //get normal
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+            
+        }
+
+        //get by id
+        //api/users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await _context.Users.FindAsync(id);
+ 
+        }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace API.Entities
+{
+	public class AppUser
+	{
+		// Set up user here
+		public int Id { get; set; }
+
+		public string UserName { get; set; }
+
+		
+	}
+}
